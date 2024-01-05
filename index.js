ebd39d2cfa5d84a95a0aee66a35e11e8e51aa2c8
Jesus M <marquez.jay444@gmail.com>
2024-01-04 18:25:21 -0600

    imports inquirer and makes questions

--- index.js
+++ index.js
@@ -1,9 +1,40 @@
 import { generateSlug } from 'random-word-slugs'
 import chalkAnimation from 'chalk-animation';
+import inquirer from 'inquirer'
 
-const length = parseInt(process.argv[2])
+inquirer.prompt([
+  {
+    type: "number",
+    name: "length",
+    message: "How long should the name be?",
+    default: 3,
+  },
+  {
+    type: "list",
+    name: "animationType",
+    message: "Select the type of animation that you desire: ",
+    default: "rainbow",
+    choices: ["rainbow","pulse","glitch","radar","neon","karaoke"]
+  }
+])
+  .then(answers => {
+    console.log(answers)
 
-const bandName = generateSlug(length, { format: 'title' })
 
+    const bandName = generateSlug(answers.length, { format: 'title' })
 
-chalkAnimation.pulse(bandName)
+    if ( answers.animationType === "pulse") {
+      chalkAnimation.pulse(bandName)
+      } else if (answers.animationType === "glitch") {
+        chalkAnimation.glitch(bandName)
+      } else if (answers.animationType === "radar") {
+        chalkAnimation.radar(bandName)
+      } else if (answers.animationType === "neon") {
+        chalkAnimation.neon(bandName)
+      } else if (answers.animationType === "rainbow") {
+        chalkAnimation.rainbow(bandName)
+      } else if (answers.animationType === "karaoke") {
+        chalkAnimation.karaoke(bandName)
+      }
+  })
+  .catch(err => console.log(err))
